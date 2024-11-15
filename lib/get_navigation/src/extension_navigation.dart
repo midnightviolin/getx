b549eb8667d3f8f6b8d343c7b524f648a27a4d8c
midnightviolin <>
2024-11-15 14:01:14 +0800

    (*) modify default color for flutter 3.22

--- lib/get_navigation/src/extension_navigation.dart
+++ lib/get_navigation/src/extension_navigation.dart
@@ -222,7 +222,8 @@ extension ExtensionDialog on GetInterface {
             child: Text(
               textConfirm ?? "Ok",
               style:
-                  TextStyle(color: confirmTextColor ?? theme.backgroundColor),
+                  TextStyle(
+                  color: confirmTextColor ?? theme.colorScheme.surface),
             ),
             onPressed: () {
               onConfirm?.call();
